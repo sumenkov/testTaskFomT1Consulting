d8addc7d2d68272c0a51f029f976f73236de3e23
sumenkov <sumenkov.dv@yandex.ru>
2023-09-14 16:13:26 +0700

    update test

--- src/test/java/ru/sumenkov/testTaskFomT1Consulting/JsonControllerTest.java
+++ src/test/java/ru/sumenkov/testTaskFomT1Consulting/JsonControllerTest.java
@@ -1,2 +1,51 @@
 package ru.sumenkov.testTaskFomT1Consulting;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import ru.sumenkov.testTaskFomT1Consulting.controller.JsonController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+public class JsonControllerTest {
+	
+	@Mock
+	private ObjectMapper objectMapper;
+	
+	private JsonController jsonController;
+	
+	@BeforeEach
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+		jsonController = new JsonController(objectMapper);
+	}
+	
+	@Test
+	public void testReturnJson() throws JsonProcessingException {
+		String inputString = "Hello World!";
+		Map<String, Integer> frequencyMap = new HashMap<>();
+		frequencyMap.put("H", 1);
+		frequencyMap.put("e", 1);
+		frequencyMap.put("l", 3);
+		frequencyMap.put("o", 2);
+		frequencyMap.put(" ", 1);
+		frequencyMap.put("W", 1);
+		frequencyMap.put("r", 1);
+		frequencyMap.put("d", 1);
+		frequencyMap.put("!", 1);
+		
+		when(objectMapper.writeValueAsString(frequencyMap)).thenReturn("{\"H\":1,\"e\":1,\"l\":3,\"o\":2,\" \":1,\"W\":1,\"r\":1,\"d\":1,\"!\":1}");
+		
+		String expectedJson = "{\"H\":1,\"e\":1,\"l\":3,\"o\":2,\" \":1,\"W\":1,\"r\":1,\"d\":1,\"!\":1}";
+		String actualJson = jsonController.returnJson(inputString);
+		
+		assertEquals(expectedJson, actualJson);
+	}
+}
